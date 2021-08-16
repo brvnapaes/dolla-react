06bcaf38068591a16bc8b642bdb6dd218aae3198
Bruna Paes <brunapaesms@gmail.com>
2021-08-16 13:22:37 -0300

    First commit

---
+++ src/components/Services/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ServicesContainer, ServicesH1, ServicesWrapper, ServicesCard, ServicesIcon, ServicesH2, ServicesP } from './ServicesElements';
+
+const Services = () => {
+    return (
+        <ServicesContainer id="services">
+            <ServicesH1>Our Services</ServicesH1>
+            <ServicesWrapper>
+                <ServicesCard>
+                    <ServicesIcon src="/images/svg-4.svg" />
+                    <ServicesH2>Reduce expenses</ServicesH2>
+                    <ServicesP>We help reduce your fees and increase your overall revenue.</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src="/images/svg-5.svg" />
+                    <ServicesH2>Virtual offices</ServicesH2>
+                    <ServicesP>You can access our platform online anywhere in the world.</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src="/images/svg-6.svg" />
+                    <ServicesH2>Premium benefits</ServicesH2>
+                    <ServicesP>Unlock our special membership card that returns 5% cash back.</ServicesP>
+                </ServicesCard>
+            </ServicesWrapper>
+        </ServicesContainer>
+    )
+}
+
+export default Services;
